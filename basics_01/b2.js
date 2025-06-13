a114783579f8b59d066d9e482b8e5c9f91079d78
Abhijeet Kumar Pandey <161673353+Abhijeetx777x@users.noreply.github.com>
2025-06-13 10:05:45 +0000

    datatypes

---
+++ basics_01/b2.js
@@ -0,0 +1,15 @@
+let name = "hitesh"; 
+let age = 81;
+let isLoggedIn = false;
+
+
+// number
+// string
+// boolean
+// null -> standalone value
+// undefined 
+// symbol
+
+//object
+
+console.log(typeof undefined );
